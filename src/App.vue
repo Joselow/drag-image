e7a5279e18c622c97bf20e334e898a2814d9ee76
Joselow <joshepmanuello.com>
2024-03-15 16:47:53 -0500

    a

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<script setup lang="ts">
+import DDImage from './components/DDImage.vue';
+interface FileI {
+    file: File,
+    name: string,
+    url: string,
+}
+
+const getImage = (data: FileI) => {
+  console.log('Lo tengo owo');
+  
+  console.log(data);  
+}
+</script>
+
+<template>
+  <div>
+    <DDImage 
+      @return="getImage"
+    />
+  </div>
+</template>
+
+<style scoped>
+</style>
